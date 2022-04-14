2da1121fc5e23e3189f9e773fc712a90ec45aee2
Armando <cuelarmando@yahoo.com>
2022-04-14 19:42:25 -0400

    added tests for contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -30,7 +30,7 @@ function ContactForm() {
     // JSX
     return (
         <section>
-            <h1>Contact Me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -49,7 +49,7 @@ function ContactForm() {
                         <p className="error-text">{errorMessage}</p>
                     </div>
                 )}
-                <button type="submit">Submit</button>
+                <button data-testid="button" utype="submit">Submit</button>
             </form>
         </section>
     )
